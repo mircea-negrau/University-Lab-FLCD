aaddc80ce51f3540136b983543caae73447208d4
Mircea Negrău <74305289+mircea-negrau@users.noreply.github.com>
2022-11-23 01:30:32 +0200

    Lab 4 (#5)
    * Initial commit
    
    * Lab 4
    
    * Create Documentation.docx

---
+++ Lab-4/Models/Transition.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab3.Models;
+
+internal sealed class Transition
+{
+    public string SourceState { get; set; }
+    public string Symbol { get; set; }
+    public string TargetState { get; set; }
+
+    public Transition(string sourceState, string symbol, string targetState)
+    {
+        SourceState = sourceState;
+        Symbol = symbol;
+        TargetState = targetState;
+    }
+
+    public override string ToString() =>
+        $"({SourceState}, {Symbol}) => {TargetState}";
+}
